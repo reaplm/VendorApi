f64b0ad23ce9f908f1d35e26534cb05d3fae8fe7
Pearl Molefe <pearlmolefe@Pearls-MacBook-Pro.local>
2023-10-05 18:39:22 +1100

    create Merchant handlers

---
+++ Vendor.Api/Handlers/GetMerchantByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MediatR;
+using Org.BouncyCastle.Asn1.Ocsp;
+using Vendor.Api.Queries;
+using Vendor.Application.Common.Interface;
+using Vendor.Domain.Entitities;
+using Vendor.Infrastructure.Persistence;
+
+namespace Vendor.Api.Handlers
+{
+    public class GetMerchantByIdHandler : IRequestHandler<GetMerchantByIdQuery, Merchant>
+    {
+        private readonly IMerchantRepository _merchantRepository;
+
+        public GetMerchantByIdHandler(IMerchantRepository merchantRepository)
+        {
+            _merchantRepository = merchantRepository;
+        }
+
+        public async Task<Merchant> Handle(GetMerchantByIdQuery request, CancellationToken cancellationToken)
+        =>  await _merchantRepository.FindById(request.Id);
+    }
+}
+
---
+++ Vendor.Api/Handlers/GetMerchantsHandler.cs
@@ -0,0 +1,23 @@
+﻿using Vendor.Api.Queries;
+using Vendor.Domain.Entitities;
+using Vendor.Application.Common.Interface;
+using MediatR;
+
+namespace Vendor.Api.Handlers
+{
+    public class GetMerchantsHandler : IRequestHandler<GetMerchantsQuery, List<Merchant>>
+    {
+		private readonly IMerchantRepository _merchantRepository;
+
+		public GetMerchantsHandler(IMerchantRepository merchantRepository)
+		{
+            _merchantRepository = merchantRepository;
+		}
+
+        public async Task<List<Merchant>> Handle(GetMerchantsQuery request,
+            CancellationToken cancellationToken) =>
+            await _merchantRepository.FindAll(request.MerchantParameters);
+
+    }
+}
+
