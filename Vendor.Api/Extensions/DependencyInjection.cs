ffb8d0fdbdd6a81e4ce3e1abad2d72f8125f4668
Pearl Molefe <pearlmolefe@Pearls-MacBook-Pro.local>
2023-10-05 18:41:54 +1100

    Vendor.Api DI

---
+++ Vendor.Api/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Vendor.Api.Queries;
+
+namespace Vendor.Api.Extensions
+{
+	public static class DependencyInjection
+	{
+		public static void AddApi(this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			//services.AddMediatR(typeof(GetMerchantsQuery));
+			//services.AddMediatR(typeof(GetMerchantByIdQuery));
+		}
+	}
+}
+
