85841266339627100577a7b6be0d43459995c122
Pearl Molefe <pearlmolefe@Pearls-MacBook-Pro.local>
2023-10-05 18:39:41 +1100

    create Merchant handlers

---
+++ Vendor.Api/Queries/GetMerchantByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+using Vendor.Domain.Entitities;
+
+namespace Vendor.Api.Queries
+{
+	public class GetMerchantByIdQuery : IRequest<Merchant>
+	{
+		public int Id { set; get; }
+	}
+}
+
---
+++ Vendor.Api/Queries/GetMerchantsQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+using Vendor.Domain.Entitities;
+using Vendor.Application.Models;
+
+namespace Vendor.Api.Queries
+{
+	public class GetMerchantsQuery : IRequest<List<Merchant>>
+    {
+		public MerchantParameters MerchantParameters { set; get; }
+
+    }
+
+}
+
